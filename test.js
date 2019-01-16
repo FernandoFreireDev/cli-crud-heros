a4fca065e25f5f7e9c5ce3ac7de92c2913994d5f
Fernando Freire <fernando.ricardo.freire@gmail.com>
2019-01-16 21:33:40 -0200

    commit inicial

---
+++ test.js
@@ -0,0 +1,28 @@
+const { deepEqual, ok } = require('assert')
+const database = require('./database')
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'speed',
+    id: 1
+}
+
+describe('Suíte de manipulação de heróis', () => {
+
+    before(async () =>{
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+
+    it('deve pesquisar um heróis usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const [resultado] = await database.listar(expected.id)
+        deepEqual(resultado, expected)
+    })
+
+    it('deve cadastrar um herói, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(actual, expected)
+    })
+})
